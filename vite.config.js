4e8176098b02569ebe35936e554ad5261be9f899
Wende Yang <105222256+Komorebi-wd@users.noreply.github.com>
2024-06-25 21:47:04 +0800

    init

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  define: {
+    'process.env': process.env
+  },
+  server: {
+    proxy: {
+      // 键值对的形式将请求代理到不同的目标服务器
+      '/test': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  }
+})
